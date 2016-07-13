908c426eee3116fd164bc743db95d59b7012b293
Spitlight <liuyiheng@hsefz.cn>
2016-07-13 22:21:04 +0800

    Add files via upload

---
+++ src/org/usfirst/frc/team9036/robot/subsystems/ballShooterSubsystem.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team9036.robot.subsystems;
+
+import org.usfirst.frc.team9036.robot.Robot;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ballShooterSubsystem extends Subsystem {
+	VictorSP collectorControl = Robot.oi.ballCollectorMotor;
+
+    public void initDefaultCommand() {
+    }
+    public void drive(double speed) {
+    	collectorControl.set(speed);
+    }
+    public void stop(){
+    	collectorControl.stopMotor();
+    }
+}
+
