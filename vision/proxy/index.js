d47356745aebbb4957f0939d510ad9fc1a8be1c1
SkyZH <iSkyZH@gmail.com>
2016-08-01 21:35:39 +0800

    remove vision files, ready for open-source

--- vision/proxy/index.js
+++
@@ -1,7 +0,0 @@
-var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
-var express = require('express');
-var app = express();
-
-app.use('/', express.static('../webdashboard'));
-app.get('/stream.mjpg', new MjpegProxy('http://localhost:8080/camera.mjpg').proxyRequest);
-app.listen(8081, "0.0.0.0");
