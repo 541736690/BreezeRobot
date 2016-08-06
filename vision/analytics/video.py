96648019079e941da6b71ab7966325f7a68aaa90
SkyZH <iSkyZH@gmail.com>
2016-08-06 14:41:43 +0800

    convert all tabs into spaces

--- vision/analytics/video.py
+++ vision/analytics/video.py
@@ -12,23 +12,23 @@ import config
 def do_capture(self):
     if capture.isOpened():
         ret, frame = capture.read()
-		if ret:
-			img, cnt = detect.do_detect(frame)
-			return img
-		else:
-			return None
-	else:
-		return None
+        if ret:
+            img, cnt = detect.do_detect(frame)
+            return img
+        else:
+            return None
+    else:
+        return None
 
 def main():
-	global capture
-	capture = cv2.VideoCapture(0)
-	capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-	capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-	try:
-		camserver.serve(config.server, do_capture)
-	except KeyboardInterrupt:
-		capture.release()
+    global capture
+    capture = cv2.VideoCapture(0)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    try:
+        camserver.serve(config.server, do_capture)
+    except KeyboardInterrupt:
+        capture.release()
 
 if __name__ == '__main__':
-	main()
+    main()
