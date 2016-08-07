1156d4273927f0e3a20d4304e0794a711e6cf036
rockazhou <rockajerryzhou@126.com>
2016-08-07 10:00:58 +0800

    directionturningoi

--- src/org/usfirst/frc/team9036/robot/OI.java
+++ src/org/usfirst/frc/team9036/robot/OI.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team9036.robot;
 
 import org.usfirst.frc.team9036.robot.commands.BallCollectorCommand;
+import org.usfirst.frc.team9036.robot.commands.DriveAdjustTurningCommand;
+import org.usfirst.frc.team9036.robot.commands.DriveDirectTurningCommand;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -15,10 +17,20 @@ public class OI {
 	
 	public Button gyroTurningAxis = new JoystickButton(mainJoystick, RobotMap.GyroTurningAxis);
 	
+	public Button forwardTurningButton = new JoystickButton(mainJoystick, RobotMap.ForwardTurningButtonID);
+	public Button backwardTurningButton = new JoystickButton(mainJoystick, RobotMap.BackwardTurningButtonID);
+	public Button leftTurningButton = new JoystickButton(mainJoystick, RobotMap.LeftTurningButtonID);
+	public Button rightTurningButton = new JoystickButton(mainJoystick, RobotMap.RightTurningButtonID);
+	
 	public Button ballCollectorButton = new JoystickButton(shootJoystick, RobotMap.BallCollectorMotorPort);
 
 	public OI() {
 		ballCollectorButton.whileHeld(new BallCollectorCommand());
+		forwardTurningButton.whenPressed(new DriveAdjustTurningCommand(0));
+		backwardTurningButton.whenPressed(new DriveAdjustTurningCommand(180));
+		leftTurningButton.whenPressed(new DriveDirectTurningCommand(-90));
+		rightTurningButton.whenPressed(new DriveDirectTurningCommand(90));
+		
 	}
 }
 
