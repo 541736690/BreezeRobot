9f5316fe181b0d1dffd999d1dd910e6c0ba28ca1
rockazhou <rockajerryzhou@126.com>
2016-08-08 11:31:04 +0800

    add camera_id

--- src/org/usfirst/frc/team9036/robot/commands/DriveDirectionChangeCommand.java
+++ src/org/usfirst/frc/team9036/robot/commands/DriveDirectionChangeCommand.java
@@ -1,16 +1,25 @@
 package org.usfirst.frc.team9036.robot.commands;
 
+import org.usfirst.frc.team9036.robot.RobotMap;
 import org.usfirst.frc.team9036.robot.subsystems.DriveSubsystem;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class DriveDirectionChangeCommand extends Command {
-
-    public DriveDirectionChangeCommand() {
+	
+	NetworkTable visionTable;
+    
+	public DriveDirectionChangeCommand() {
     }
 
     protected void initialize() {
     	DriveSubsystem.driveDirection = DriveSubsystem.driveDirection * -1;
+    	if (DriveSubsystem.driveDirection == 1){
+    		visionTable.putNumber("camera_id", 1);
+    	} else if ( DriveSubsystem.driveDirection == -1){
+    		visionTable.putNumber("camera_id", -1);
+    	}
     }
 
     protected void execute() {
